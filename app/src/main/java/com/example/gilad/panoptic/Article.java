3e6cb4e038a44b3c45fc7babaadbb2e913d69cc4
giladstern <gilad.stern91@gmail.com>
2017-08-11 17:13:29 +0300

    Halfway

---
+++ app/src/main/java/com/example/gilad/panoptic/Article.java
@@ -0,0 +1,18 @@
+package com.example.gilad.panoptic;
+
+/**
+ * Created by Gilad on 8/11/2017.
+ */
+public class Article {
+    public String title;
+    public String url;
+    public String img;
+    public String source;
+
+    public Article(String title, String url, String img, String source) {
+        this.title = title;
+        this.url = url;
+        this.img = img;
+        this.source = source;
+    }
+}
--- app/src/main/java/com/example/gilad/panoptic/MainActivity.java
+++ app/src/main/java/com/example/gilad/panoptic/MainActivity.java
@@ -1,5 +1,6 @@
     package com.example.gilad.panoptic;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Fetcher.collect();
     }
 }
