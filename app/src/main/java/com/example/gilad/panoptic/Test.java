5b84b3ffcf216cdddb151f35c246da1dacc47b6a
giladstern <gilad.stern91@gmail.com>
2017-08-18 15:28:09 +0300

    Article and cluster classes

--- app/src/main/java/com/example/gilad/panoptic/Test.java
+++ app/src/main/java/com/example/gilad/panoptic/Test.java
@@ -12,6 +12,8 @@ import com.amazonaws.regions.Regions;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.util.List;
+
 
 public class Test extends AppCompatActivity {
 
@@ -56,17 +58,16 @@ public class Test extends AppCompatActivity {
                     return;
                 }
 
-                JSONArray jsonArray = null;
+                List<Cluster> clusters = null;
 
                 try {
-                    String data = result.getData().replace("u'", "'").replace("u\"","\"");
-                    jsonArray = new JSONArray(data);
+                    clusters = Cluster.parseClusters(result.getData());
                 } catch(JSONException e){
                     Log.d("Debug", e.getMessage());
                 }
 
-                if (jsonArray != null) {
-                    Log.d("Debug", jsonArray.toString());
+                if (clusters != null) {
+                    Log.d("Debug", clusters.toString());
                 } else {
                     Log.d("Debug", "NULL!");
                 }
